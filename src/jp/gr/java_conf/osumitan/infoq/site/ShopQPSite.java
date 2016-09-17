e5b63a2cdfe00ff502248870fa56142d3a028754
osumitan <osumitan@gmail.com>
2016-09-17 23:08:41 +0900

    shop-pq renewal

--- src/jp/gr/java_conf/osumitan/infoq/site/ShopQPSite.java
+++ src/jp/gr/java_conf/osumitan/infoq/site/ShopQPSite.java
@@ -15,32 +15,36 @@ public class ShopQPSite extends EnqueteSite {
 		// ドメイン
 		super.domain = "enq.shop-qp.com";
 		// 次へボタンセレクタ
-		super.nextButtonSelector = By.xpath("//*[@id='btnlarge' or contains(@class,'ui-button')]");
+		super.nextButtonSelector = By.xpath("//*[@id='btnlarge' or contains(@class,'adnextbtn')]");
 		// 最終テキストパス
 		super.finalTextPath = By.xpath("//p[contains(text(),'ご回答ありがとうございます。')]");
 		// 最終ボタンセレクタ
 		super.finalButtonSelector = super.nextButtonSelector;
 		// ラジオボタンセレクタ
-		super.radioButtonSelector = By.cssSelector("label[for^='radio']");
+		super.radioButtonSelector = By.cssSelector("label[for^='q']");
 		// チェックボックスセレクタ
-		super.checkBoxSelector = By.cssSelector("label[for^='check']");
+		super.checkBoxSelector = By.cssSelector("label[for^='q']");
 		// オプションセレクタ
-		super.optionSelector = By.cssSelector("select[name^='qa'] option");
+		super.optionSelector = By.cssSelector("select[name^='q'] option");
 		// 性別質問パス
-		super.genderQuestionPath = By.xpath("//h2[contains(text(),'性別をお知らせ下さい。')]");
+		super.genderQuestionPath = By.xpath("//div[contains(text(),'性別をお知らせください。')]");
 		// 性別回答パス
 		super.genderAnswerPath = By.xpath("//label[contains(text(),'男性')]");
 		// 年齢質問パス
-		super.ageQuestionPath = By.xpath("//h2[contains(text(),'年齢をお知らせ下さい。')]");
+		super.ageQuestionPath = By.xpath("//div[contains(text(),'年齢をお知らせください。')]");
 		// 年齢回答パス
-		super.ageAnswerPath = By.xpath("//label[contains(text(),'40代')]");
+		super.ageAnswerPath = By.xpath("//label[contains(text(),'40〜49歳')]");
 		// 居住地質問パス
-		super.residenceQuestionPath = By.xpath("//h2[contains(text(),'居住区をお知らせ下さい。')]");
+		super.residenceQuestionPath = By.xpath("//div[contains(text(),'都道府県をお知らせください。')]");
 		// 居住地回答パス
-		super.residenceAnswerPath = By.xpath("//select[@name='qa']/option[contains(text(),'関東')]");
+		super.residenceAnswerPath = By.xpath("//select[contains(@name,'q')]/option[contains(text(),'千葉県')]");
 		// 職業質問パス
-		super.jobQuestionPath = By.xpath("//h2[contains(text(),'ご職業をお知らせ下さい。')]");
+		super.jobQuestionPath = By.xpath("//div[contains(text(),'職業をお知らせください。')]");
 		// 職業回答パス
-		super.jobAnswerPath = By.xpath("//label[contains(text(),'会社員')]");
+		super.jobAnswerPath = By.xpath("//label[contains(text(),'会社勤務（一般社員）')]");
+		// フロート広告クローズボタンセレクタ
+		super.floatAdCloseButtonSelector = By.cssSelector("div.bnrclose");
+		// フロート広告クローズスクリプト
+		super.floatAdCloseScript = "$('div.bnrclose').click();";
 	}
 }
