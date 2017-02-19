74783f7637e1c0da16080c99a3ddfca9278dfd66
osumitan <osumitan@gmail.com>
2017-02-19 19:07:10 +0900

    enq.nstk-4.com added

--- src/jp/gr/java_conf/osumitan/infoq/site/Y2atSite.java
+++ src/jp/gr/java_conf/osumitan/infoq/site/Y2atSite.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 import org.openqa.selenium.By;
 
 /**
- * アンケートサイト：enq.y2at.com
+ * アンケートサイト：enq.y2at.com | enq.nstk-4.com
 */
 public class Y2atSite extends EnqueteSite {
 
@@ -17,7 +17,7 @@ public class Y2atSite extends EnqueteSite {
 		// ドメイン
 		super.domain = "enq.y2at.com";
 		// ドメインパターン
-		super.domainPattern = Pattern.compile("^.*enq\\.y2at\\.com.*$");
+		super.domainPattern = Pattern.compile("^.*enq\\.(y2at|nstk-4)\\.com.*$");
 		// スタートボタンセレクタ
 		super.startButtonSelector = By.cssSelector("#btnlarge");
 		// 次へボタンセレクタ
@@ -33,19 +33,19 @@ public class Y2atSite extends EnqueteSite {
 		// 性別質問パス
 		super.genderQuestionPath = By.xpath("//div[contains(text(),'あなたの性別をお知らせください。')]");
 		// 性別回答パス
-		super.genderAnswerPath = By.xpath("//label[starts-with(@for,'q') and text()='男性']/ancestor::div/span/input[@type='radio']");
+		super.genderAnswerPath = By.xpath("//label[starts-with(@for,'q') and contains(text(),'男性')]/ancestor::div/span/input[@type='radio']");
 		// 年齢質問パス
 		super.ageQuestionPath = By.xpath("//div[contains(text(),'あなたの年齢をお知らせください。')]");
 		// 年齢回答パス
-		super.ageAnswerPath = By.xpath("//label[starts-with(@for,'q') and text()='40〜49歳']/ancestor::div/span/input[@type='radio']");
+		super.ageAnswerPath = By.xpath("//label[starts-with(@for,'q') and contains(text(),'40〜49歳')]/ancestor::div/span/input[@type='radio']");
 		// 居住地質問パス
 		super.residenceQuestionPath = By.xpath("//div[contains(text(),'あなたのお住まいの都道府県をお知らせください。')]");
 		// 居住地回答パス
-		super.residenceAnswerPath = By.xpath("//select[starts-with(@name,'q')]/option[text()='千葉県']");
+		super.residenceAnswerPath = By.xpath("//select[starts-with(@name,'q')]/option[contains(text(),'千葉県')]");
 		// 職業質問パス
 		super.jobQuestionPath = By.xpath("//div[contains(text(),'あなたの職業をお知らせください。')]");
 		// 職業回答パス
-		super.jobAnswerPath = By.xpath("//label[starts-with(@for,'q') and text()='会社勤務（一般社員）']/ancestor::div/span/input[@type='radio']");
+		super.jobAnswerPath = By.xpath("//label[starts-with(@for,'q') and contains(text(),'会社勤務（一般社員）')]/ancestor::div/span/input[@type='radio']");
 		// フロート広告クローズボタンセレクタ
 		super.floatAdCloseButtonSelector = By.cssSelector("div.bnrclose");
 		// フロート広告クローズスクリプト
